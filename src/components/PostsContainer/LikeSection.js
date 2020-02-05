810d1406b858f619dd8a47aead64b1b0cbc79d0a
kkdev211 <kkdev211@users.noreply.github.com>
2020-02-04 20:57:15 -0500

    likes

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -1,8 +1,13 @@
-import React, {useState } from 'react';
+import React, { useState } from 'react';
 
 const LikeSection = props => {
   
-  const [likes, setLikes] =useState();
+  const [likes, setLikes] =useState(0);
+  const handleLike = () => {
+    // props.setLikes(props.likes + 1);
+    // alert(`${props.likes + 1}`)
+  };
+  
   return (
     <div>
     <div
@@ -10,7 +15,7 @@ const LikeSection = props => {
       key="likes-icons-container"
     >
       <div className="like-section-wrapper">
-        <i className="far fa-heart" />
+        <i className="far fa-heart" onClick={handleLike}/>
       </div>
       <div className="like-section-wrapper">
         <i className="far fa-comment" />
@@ -21,9 +26,14 @@ const LikeSection = props => {
 </div>
   )
 };
-function LikeButton(props) {
-  const {likes} = props;
-  return 
-}
+// function LikeButton(props) {
+//   const {likes} = props;
+//   return 
+// }
+// var heartBtn = document.querySelector(".fa-heart")
+// heartBtn.addEventListener('click', () => {
+//   alert("yay");
+    
+// })
 
 export default LikeSection;
